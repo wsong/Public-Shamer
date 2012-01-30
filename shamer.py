a7fe39b7eb36237ab6c219ccd45122694b2e99fb
Wayne Song <wayne.e.song@gmail.com>
2012-01-30 14:42:28 -0800

    More robust url encoding

--- shamer.py
+++ shamer.py
@@ -7,8 +7,8 @@ import urlparse
 APP_ID = ""
 REDIRECT_URI = ""
 APP_SECRET = ""
-OAUTH_DIALOG_URL = "https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=%s&scope=publish_stream,offline_access" % (APP_ID, REDIRECT_URI)
-ACCESS_TOKEN_URL = "https://graph.facebook.com/oauth/access_token?client_id=%s&redirect_uri=%s&client_secret=%s" % (APP_ID, REDIRECT_URI, APP_SECRET)
+OAUTH_DIALOG_URL = "https://www.facebook.com/dialog/oauth?" + urllib.urlencode({"client_id": APP_ID, "redirect_uri": REDIRECT_URI, "scope": "publish_stream,offline_access"})
+ACCESS_TOKEN_URL = "https://graph.facebook.com/oauth/access_token?"
 
 urls = (
     '/', 'index'
@@ -21,7 +21,8 @@ class index:
     def GET(self):
         i = web.input(code=None)
         if i.code:
-            access_url = ACCESS_TOKEN_URL + "&code=%s" % (i.code)
+            args = urllib.urlencode({"client_id": APP_ID, "redirect_uri": REDIRECT_URI, "client_secret": APP_SECRET, "code": i.code})
+            access_url = ACCESS_TOKEN_URL + args
             response = urllib2.urlopen(access_url).read()
             response_dict = urlparse.parse_qs(response)
             access_token = response_dict['access_token'][0]
