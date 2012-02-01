048a55432dea79c36812fc45aeab709b3ee2d1e7
Wayne Song <wayne.e.song@gmail.com>
2012-02-01 01:14:58 -0800

    Fixed a few typos

--- fbgraph.py
+++ fbgraph.py
@@ -13,14 +13,14 @@ ACCESS_TOKEN_URL = "https://graph.facebook.com/oauth/access_token?"
 CURRENT_USER_URL = "https://graph.facebook.com/me?"
 
 def get_access_token(code):
-    args = urllib.urlencode({"client_id": APP_ID, "redirect_uri": REDIRECT_URI, "client_secret": APP_SECRET, "code": i.code})
+    args = urllib.urlencode({"client_id": APP_ID, "redirect_uri": REDIRECT_URI, "client_secret": APP_SECRET, "code": code})
     access_url = ACCESS_TOKEN_URL + args
     response = urllib2.urlopen(access_url).read()
     response_dict = urlparse.parse_qs(response)
     return response_dict['access_token'][0]
 
 def get_current_user_info(access_token):
-    args = urllib.urlencode({"access_token", access_token})
+    args = urllib.urlencode({"access_token": access_token})
     current_user_url = CURRENT_USER_URL + args
     response = urllib2.urlopen(current_user_url).read()
-    return response
+    return urlparse.parse_qs(response)
