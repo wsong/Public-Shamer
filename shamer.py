d6841d35b1623252f870e663660b5aefa5a7ee53
Wayne Song <wayne.e.song@gmail.com>
2012-02-01 02:06:48 -0800

    Added database support to main file

--- shamer.py
+++ shamer.py
@@ -2,6 +2,7 @@
 
 import web
 import fbgraph
+import shamerdb
 
 urls = (
     '/', 'index'
@@ -15,11 +16,18 @@ class index:
         i = web.input(code=None)
         if i.code:
             access_token = fbgraph.get_access_token(i.code)
-            open(".access_token", "w").write(access_token)
+            user_dict = fbgraph.get_current_user_info(access_token)
+            if not shamerdb.get_user_by_fb_id(user_dict["id"][0]):
+                shamerdb.create_user(user_dict["id"][0],
+                                     user_dict["name"][0],
+                                     user_dict["first_name"][0],
+                                     user_dict["last_name"][0],
+                                     access_token)
             return render.index("")
         else:
             return render.index(OAUTH_DIALOG_URL)
             
 
 if __name__ == "__main__":
+    shamerdb.database_init()
     app.run()
