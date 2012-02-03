9a4fb500bca690907da6316582f0cac011261c3a
Wayne Song <wayne.e.song@gmail.com>
2012-02-02 23:43:37 -0800

    Except this time, maybe I'll remember to actually save the file

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ FB_APP_ID = ""
 FB_REDIRECT_URI = ""
 FB_APP_SECRET = ""
 
-FB_OAUTH_DIALOG_URL = "https://www.facebook.com/dialog/oauth?" + urllib.urlencode({"client_id": APP_ID, "redirect_uri": REDIRECT_URI, "scope": "publish_stream,offline_access"})
+FB_OAUTH_DIALOG_URL = "https://www.facebook.com/dialog/oauth?"
 FB_ACCESS_TOKEN_URL = "https://graph.facebook.com/oauth/access_token?"
 FB_CURRENT_USER_URL = "https://graph.facebook.com/me?"
 FB_FEED_URL = "https://graph.facebook.com/feed"
