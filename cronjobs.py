cbe2ecd3ec70e19929cdb6b0cc73014876b29dad
Wayne Song <wayne.e.song@gmail.com>
2012-02-03 14:41:25 -0800

    Forgot an argument in repr

--- cronjobs.py
+++ cronjobs.py
@@ -29,7 +29,7 @@ class CrontabLine:
             self.fb_id = fb_id
             self.username = username
             self.service = service
-    def __repr__():
+    def __repr__(self):
         return "* %d * * %d %s %s %s %s" % (hour, dayofweek, command, fb_id,
                                             username, service)
 
