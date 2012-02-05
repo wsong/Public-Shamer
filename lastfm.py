515d938fe26597826a484a9458b08df4e70eb974
Wayne Song <wayne.e.song@gmail.com>
2012-02-05 15:14:02 -0800

    Made adding beratements easier

--- lastfm.py
+++ lastfm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import constants
+import random
 import time
 import urllib2
 from xml.dom import minidom
@@ -23,14 +24,41 @@ def get_lastfm_weekly_playcount(user):
         total += int(node.getElementsByTagName("playcount")[0].firstChild.nodeValue)
     return total
 
+def get_very_low_evaluation(count):
+    message_list = [
+        "I listened to %d songs this week, which is just breathtakingly low.  My top three artists are the Beatles, Linkin Park, and, hrm, the last one is a tie between the Eagles and... did I already say the Beatles?" % (count),]
+    return random.choice(message_list)
+
+def get_low_evaluation(count):
+    message_list = [
+        "I listened to %d songs this week, which is like a single album.  Well, if you count %d repetitions of \"Like a G6\" as an album." % (count, count),]
+    return random.choice(message_list)
+
+def get_medium_low_evaluation(count):
+    message_list = [
+        "I listened to %d songs this week, which is so close to being a good length of music (unless it's %d Dream Theater songs, in which case I guess I listened to like %d hours of music)." % (count, count, count*3),]
+    return random.choice(message_list)
+
+def get_medium_evaluation(count):
+    message_list = [
+        "I listened to %d songs this week; sounds like I had a good time (oh no my social life D:)." % (count),]
+    return random.choice(message_list)
+
+def get_default_evaluation(count):
+    message_list = [
+        "On the downside, I was never able to take my headphones out this week.  On the upside, I listened to %d songs." % (count),]
+    return random.choice(message_list)
+
+    
 def get_weekly_evaluation(count):
+    message_list = None
     if count <= 10:
-        return "I listened to %d songs this week, which is just breathtakingly low.  My top three artists are the Beatles, Linkin Park, and, hrm, the last one is a tie between the Eagles and... did I already say the Beatles?" % (count)
+        return get_very_low_evaluation(count)
     elif count <= 20:
-        return "I listened to %d songs this week, which is like a single album.  Well, if you count %d repetitions of \"Like a G6\" as an album." % (count, count)
+        return get_low_evaluation(count)
     elif count <= 40:
-        return "I listened to %d songs this week, which is so close to being a good length of music (unless it's %d Dream Theater songs, in which case I guess I listened to like %d hours of music)." % (count, count, count*3)
+        return get_medium_low_evaluation(count)
     elif count <= 70:
-        return "I listened to %d songs this week; sounds like I had a good time (oh no my social life D:)." % (count)
+        return get_medium_evaluation(count)
     else:
-        return "On the downside, I was never able to take my headphones out this week.  On the upside, I listened to %d songs." % (count)
+        return get_default_evaluation(count)
