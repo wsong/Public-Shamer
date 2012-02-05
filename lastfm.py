0de84451f08fb0e65893be1a224ece60ea10ff03
Wayne Song <wayne.e.song@gmail.com>
2012-02-05 15:46:16 -0800

    Added more snark

--- lastfm.py
+++ lastfm.py
@@ -26,22 +26,30 @@ def get_lastfm_weekly_playcount(user):
 
 def get_very_low_evaluation(count):
     message_list = [
-        "I listened to %d songs this week, which is just breathtakingly low.  My top three artists are the Beatles, Linkin Park, and, hrm, the last one is a tie between the Eagles and... did I already say the Beatles?" % (count),]
+        "I listened to %d songs this week, which is just breathtakingly low.  Myn top three artists are the Beatles, Linkin Park, and, hrm, the last one is a tie between the Eagles and... did I already say the Beatles?" % (count),
+        "This week, I listened to %d songs.  Kinda makes you wonder if I actually enjoy music, or if I just like the way earbuds feel." % (count),
+        "Over the last seven days, I listened to %d songs.  I'm not actually into music, but I bought this iPod to look cooler on the subway and damned if I'm not using it." % (count),]
     return random.choice(message_list)
 
 def get_low_evaluation(count):
     message_list = [
-        "I listened to %d songs this week, which is like a single album.  Well, if you count %d repetitions of \"Like a G6\" as an album." % (count, count),]
+        "I listened to %d songs this week, which is like a single album.  Well, if you count %d repetitions of \"Like a G6\" as an album." % (count, count),
+        "I, like, totally had LMFAO stuck in my head this week, and I listened to them like %d times." % (count),
+        "I listened to %d song this week.  I would've listened to more, but NPR only publishes so many songs." % (count),]
     return random.choice(message_list)
 
 def get_medium_low_evaluation(count):
     message_list = [
-        "I listened to %d songs this week, which is so close to being a good length of music (unless it's %d Dream Theater songs, in which case I guess I listened to like %d hours of music)." % (count, count, count*3),]
+        "I listened to %d songs this week, which is so close to being a good length of music (unless it's %d Dream Theater songs, in which case I guess I listened to like %d hours of music)." % (count, count, count*3),
+        "This week, I listened to %d songs.  I wanna listen to more music, but it's so hard to afford it all on iTunes.  99 cents is expensive!" % (count),
+        "Over the last seven days, I listened to %d songs.  I'm really into music, but sometimes I wonder if what I'm really into is sounding like I'm into music." % (count),]
     return random.choice(message_list)
 
 def get_medium_evaluation(count):
     message_list = [
-        "I listened to %d songs this week; sounds like I had a good time (oh no my social life D:)." % (count),]
+        "I listened to %d songs this week; sounds like I had a good time (oh no my social life D:)." % (count),
+        "I listened to %d songs over the last seven days; I'm really into Grizzly Bear, Ariel Pink, Wavves and... oh God, all my Pitchfork references are out of date." % (count),
+        "Over the last seven days, I listened to %d songs; more or less where I wanna be in my listening career (well, except that %d of those were dubstep)." % (count, count),]
     return random.choice(message_list)
 
 def get_default_evaluation(count):
@@ -51,7 +59,6 @@ def get_default_evaluation(count):
 
     
 def get_weekly_evaluation(count):
-    message_list = None
     if count <= 10:
         return get_very_low_evaluation(count)
     elif count <= 20:
