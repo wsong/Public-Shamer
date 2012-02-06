e5b3837a8141f1725cdc1def151ff26cca8f6920
Wayne Song <wayne.e.song@gmail.com>
2012-02-06 12:16:17 -0800

    Added Nike+ snark

--- nikeplus.py
+++ nikeplus.py
@@ -36,19 +36,43 @@ def get_weekly_run_total(user_id):
 
 def get_very_low_evaluation(distance):
     message_list = [
-        "This week, I ran %f miles; that moving sidewalk I installed is basically paying for itself." % (distance),
+        "This week, I ran %.2f miles; that moving sidewalk I installed is basically paying for itself." % (distance),
+        ]
+    return random.choice(message_list)
+
+def get_low_evaluation(distance):
+    message_list = [
+        "This week I ran %.2f miles, which is really not that much.  I mean, did I run on my hands or something?" % (distance),
+        ]
+    return random.choice(message_list)
+
+def get_medium_low_evaluation(distance):
+    message_list = [
+        "Over the last seven days, I ran %.2f miles, which isn't exactly the distance from Marathon to Athens.  If we're being honest, it's really more like the distance from my apartment to the corner store." % (distance),
+        ]
+    return random.choice(message_list)
+
+def get_medium_evaluation(distance):
+    message_list = [
+        "Over the last week, I ran %.2f miles.  Pretty respectable, I'd say; just a few more miles a week and I'll have justified my five-toed shoes purchase." % (distance),
         ]
     return random.choice(message_list)
 
 def get_default_evaluation(distance):
     message_list = [
-        "I ran %f miles this week.  If everyone was like me, we could solve the energy crisis by just running everywhere." % (distance),
+        "I ran %.2f miles this week.  If everyone was like me, we could solve the energy crisis by just running everywhere." % (distance),
         ]
     return random.choice(message_list)
 
 def get_weekly_evaluation(distance):
     miles = distance*0.621371192
-    if miles <= 1:
+    if miles <= 1.0:
         return get_very_low_evaluation(miles)
+    elif miles <= 3.0:
+        return get_low_evaluation(miles)
+    elif miles <= 7.0:
+        return get_medium_low_evaluation(miles)
+    elif miles <= 12.0:
+        return get_medium_evaluation(miles)
     else:
         return get_default_evaluation(miles)
